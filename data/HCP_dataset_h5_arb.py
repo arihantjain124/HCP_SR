72b8392b1ccde1e1303c0a21fccb49ef1237e786
arihantjain124 <arihantjain124@outlook.com>
2023-11-22 13:54:43 +0000

    Working Dataloader next step dynamic scaling each itera

--- data/HCP_dataset_h5_arb.py
+++ data/HCP_dataset_h5_arb.py
@@ -15,7 +15,7 @@ np.seterr(all="ignore")
 
 loaded = {}
 loaded_gt ={}
-shape_3t = [145,174,174]
+shape_3t = [145,174,145]
 shape_7t = [173,207,173]
 shape_ratio = [round(shape_7t[i]/shape_3t[i],1) for i in range(3)]
 path,tot = "",""
@@ -115,12 +115,12 @@ class hcp_data(torch.utils.data.Dataset):
         return self.blk_indx[-1]
     
     def set_scale(self, scale,blk_size):
-        # if(scale == self.scale and blk_size == self.blk_size):
-        #     pass
-        # else:
-        self.scale = scale
-        self.blk_size = blk_size
-        self.preload_data()
+        if(scale == self.scale and blk_size == self.blk_size):
+            pass
+        else:
+            self.scale = scale
+            self.blk_size = blk_size
+            self.preload_data()
         
     def __getitem__(self,indx):
 
@@ -148,33 +148,30 @@ class hcp_data(torch.utils.data.Dataset):
         self.blk_indx = np.cumsum(self.blk_indx)
 
     
-    def blk_points_pair(self,datalr,datahr,blk_size = [16,16,4],sca = (1,1,1)):
+    def blk_points_pair(self,datalr,datahr,blk_size = [16,16,4],sca = (1,1,1),stride = (0,0,0)):
     
         shpind = torch.nonzero(datalr)
         xmin,xmax = torch.min(shpind[:,0]).item(),torch.max(shpind[:,0]).item()
         ymin,ymax = torch.min(shpind[:,1]).item(),torch.max(shpind[:,1]).item()
         zmin,zmax = torch.min(shpind[:,1]).item(),torch.max(shpind[:,2]).item()
-        
-        len_lr = [xmax - xmin + 1,ymax - ymin + 1,zmax - zmin + 1]
-        n = [int(round(len_lr[i] / blk_size[i])) for i in range(3)]
-        
-        # determine starting and ending indices of each block
-        
+
         lr_start = [xmin,ymin,zmin]
         lr_end = [xmax - blk_size[0] + 1,ymax - blk_size[1] + 1,zmax - blk_size[2] + 1]
-        
+
         shpind = torch.nonzero(datahr)
         xmin,xmax = torch.min(shpind[:,0]).item(),torch.max(shpind[:,0]).item()
         ymin,ymax = torch.min(shpind[:,1]).item(),torch.max(shpind[:,1]).item()
         zmin,zmax = torch.min(shpind[:,1]).item(),torch.max(shpind[:,2]).item()
-        
+
         blk_size_hr = [round(blk_size[i]*sca[i]) for i in range(3)]
         hr_start = [xmin,ymin,zmin]
         hr_end = [xmax - blk_size_hr[0] + 1,ymax - blk_size_hr[1] + 1,zmax - blk_size_hr[2] + 1]
-#         print(datalr.shape,datahr.shape)
-        ranges_lr = [np.round(np.linspace(lr_start[i], lr_end[i], n[i])) for i in range(3)]
-        ranges_hr = [np.round(np.linspace(hr_start[i], hr_end[i], n[i])) for i in range(3)]
-        
+
+
+        a,b = [lr_end[i] - lr_start[i] for i in range(3)],[hr_end[i] - hr_start[i] for i in range(3)]
+        offset = [round(b[i]/a[i],1) for i in range(3)]
+        ranges_lr = [np.arange(lr_start[i], lr_end[i], blk_size[i] - stride[i]) for i in range(3)]
+        ranges_hr = [np.round(ranges_lr[i]*offset[i]) for i in range(3)]
         
         ind_block_lr = []
         ind_block_hr = []
@@ -183,12 +180,12 @@ class hcp_data(torch.utils.data.Dataset):
         for ii in np.arange(0, ranges_lr[0].shape[0]):
             for jj in np.arange(0, ranges_lr[1].shape[0]):
                 for kk in np.arange(0, ranges_lr[2].shape[0]):
-                    x,y,z = ranges_lr[0][ii],ranges_lr[1][ii],ranges_lr[2][ii]
+                    x,y,z = ranges_lr[0][ii],ranges_lr[1][jj],ranges_lr[2][kk]
                     temp_lr = np.array([x, x + blk_size[0]-1, 
                                         y, y + blk_size[1]-1, 
                                         z, z + blk_size[2]-1]).astype(int)
                     
-                    x,y,z = ranges_hr[0][ii],ranges_hr[1][ii],ranges_hr[2][ii]
+                    x,y,z = ranges_hr[0][ii],ranges_hr[1][jj],ranges_hr[2][kk]
                     temp_hr = np.array([x, x + blk_size_hr[0]-1,
                                         y, y + blk_size_hr[1]-1,
                                         z, z + blk_size_hr[2]-1]).astype(int)
@@ -209,7 +206,7 @@ class hcp_data(torch.utils.data.Dataset):
         ind_block_hr = np.stack(ind_block_hr)
         ind_block_hr = ind_block_hr.astype(int)
         # print(ind_block)
-        return ind_block_lr,ind_block_hr,len(ind_block_lr),ranges_lr,ranges_hr
+        return ind_block_lr,ind_block_hr,len(ind_block_lr)
 
 
     def extract_block(self,data, inds):
@@ -247,17 +244,14 @@ class hcp_data(torch.utils.data.Dataset):
 class hcp_data_test_recon(torch.utils.data.Dataset):
     def __init__(self, opt,ids,test = False):
         super(hcp_data).__init__()
-        if(test == True):
-            self.blk_size = opt.test_block_size
-        else:
-            self.blk_size = opt.block_size
+        self.blk_size = opt.test_size
         self.ret_points = opt.ret_points
-        self.stride = opt.stride
-        self.thres = 0.1
+        self.thres = opt.thres
         self.base_dir = opt.dir if opt.dir != None else "/storage/users/arihant"
         self.ids = ids
         self.debug = opt.debug
         self.scale = opt.scale
+        self.stride = opt.stride
         if(opt.sort == True):
             self.ids.sort()
         self.preload_data()
@@ -279,7 +273,7 @@ class hcp_data_test_recon(torch.utils.data.Dataset):
         vol_idx = self.ids[indx]
         
         if(self.ret_points):
-            return self.loaded_blk[vol_idx],self.loaded_fa[vol_idx],self.loaded_adc[vol_idx],self.loaded_rgb[vol_idx],self.loaded_ret_hr[vol_idx],self.loaded_mask[vol_idx]
+            return self.loaded_blk[vol_idx],self.loaded_adc[vol_idx],self.loaded_fa[vol_idx],self.loaded_rgb[vol_idx],self.loaded_ret_hr[vol_idx],self.loaded_mask[vol_idx],self.loaded_ret_lr[vol_idx]
         else:
             return self.loaded_blk[vol_idx],self.loaded_adc[vol_idx],self.loaded_fa[vol_idx],self.loaded_rgb[vol_idx]
         
@@ -290,118 +284,107 @@ class hcp_data_test_recon(torch.utils.data.Dataset):
         self.loaded_fa = {}
         self.loaded_rgb = {}
         self.loaded_ret_hr = {}
+        self.loaded_ret_lr = {}
         self.loaded_mask = {}
         for i in self.ids:
-            self.loaded_blk[i],self.loaded_adc[i],self.loaded_fa[i],self.loaded_rgb[i],self.loaded_ret_hr[i],self.loaded_mask[i] = self.pre_proc(i)
+            self.loaded_blk[i],self.loaded_adc[i],self.loaded_fa[i],self.loaded_rgb[i],self.loaded_ret_hr[i],self.loaded_mask[i],self.loaded_ret_lr[i]  = self.pre_proc(i)
             if(self.debug == True):
                 print(i,"loaded")
 
 
-        
-    def blk_points_pair(self,datalr,datahr,blk_size = [16,16,4],sca = (1,1,1),stride = (2,2,2),thres = 0.4):
+    def blk_points_pair(self,datalr,datahr,blk_size = [16,16,4],sca = (1,1,1),stride = (0,0,0)):
+    
+        shpind = torch.nonzero(datalr)
+        xmin,xmax = torch.min(shpind[:,0]).item(),torch.max(shpind[:,0]).item()
+        ymin,ymax = torch.min(shpind[:,1]).item(),torch.max(shpind[:,1]).item()
+        zmin,zmax = torch.min(shpind[:,1]).item(),torch.max(shpind[:,2]).item()
 
-
-        shpind = np.nonzero(datalr)
-        xmin,xmax = np.min(shpind[0]),np.max(shpind[0])
-        ymin,ymax = np.min(shpind[1]),np.max(shpind[1])
-        zmin,zmax = np.min(shpind[2]),np.max(shpind[2])
-        
-        len_lr = [xmax - xmin + 1,ymax - ymin + 1,zmax - zmin + 1]
-
-        n = [int(round(len_lr[i] / (blk_size[i] - stride[i]))) for i in range(3)]
-
-        # determine starting and ending indices of each block
-        
-        lr_start = [xmin - stride[0],ymin - stride[1],zmin - stride[2]]
+        lr_start = [xmin,ymin,zmin]
         lr_end = [xmax - blk_size[0] + 1,ymax - blk_size[1] + 1,zmax - blk_size[2] + 1]
 
+        shpind = torch.nonzero(datahr)
+        xmin,xmax = torch.min(shpind[:,0]).item(),torch.max(shpind[:,0]).item()
+        ymin,ymax = torch.min(shpind[:,1]).item(),torch.max(shpind[:,1]).item()
+        zmin,zmax = torch.min(shpind[:,1]).item(),torch.max(shpind[:,2]).item()
 
-
-        shpind = np.nonzero(datahr)
-        xmin,xmax = np.min(shpind[0]),np.max(shpind[0])
-        ymin,ymax = np.min(shpind[1]),np.max(shpind[1])
-        zmin,zmax = np.min(shpind[2]),np.max(shpind[2])
-
-        
         blk_size_hr = [round(blk_size[i]*sca[i]) for i in range(3)]
-
-        hr_start = [xmin - stride[0],ymin - stride[1],zmin - stride[2]]
+        hr_start = [xmin,ymin,zmin]
         hr_end = [xmax - blk_size_hr[0] + 1,ymax - blk_size_hr[1] + 1,zmax - blk_size_hr[2] + 1]
 
-        ranges_lr = [np.round(np.linspace(lr_start[i], lr_end[i], n[i])) for i in range(3)]
-        ranges_hr = [np.round(np.linspace(hr_start[i], hr_end[i], n[i])) for i in range(3)]
+
+        a,b = [lr_end[i] - lr_start[i] for i in range(3)],[hr_end[i] - hr_start[i] for i in range(3)]
+        offset = [round(b[i]/a[i],1) for i in range(3)]
+        ranges_lr = [np.arange(lr_start[i], lr_end[i], blk_size[i] - stride[i]) for i in range(3)]
+        ranges_hr = [np.round(ranges_lr[i]*offset[i]) for i in range(3)]
         
         ind_block_lr = []
         ind_block_hr = []
         count = 0
-        
-        offset = [round((blk_size_hr[i] / shape_ratio[i])-blk_size[i]) for i in range(3)]
-        offset = [[i//2,i//2] if i%2==0 else [i//2,i//2+1] for i in offset]
-        offset = [i if i[0]>0 else [0,0] for i in offset]
-        # print(offset)
-
 
         for ii in np.arange(0, ranges_lr[0].shape[0]):
             for jj in np.arange(0, ranges_lr[1].shape[0]):
                 for kk in np.arange(0, ranges_lr[2].shape[0]):
-
-                    temp_lr = np.array([ranges_lr[0][ii] - offset[0][0], ranges_lr[0][ii]+blk_size[0]-1 + offset[0][1], 
-                                        ranges_lr[1][jj] - offset[1][0], ranges_lr[1][jj]+blk_size[1]-1 + offset[1][1], 
-                                        ranges_lr[2][kk] - offset[2][0], ranges_lr[2][kk]+blk_size[2]-1 + offset[2][1]]).astype(int)
+                    x,y,z = ranges_lr[0][ii],ranges_lr[1][jj],ranges_lr[2][kk]
+                    temp_lr = np.array([x, x + blk_size[0]-1, 
+                                        y, y + blk_size[1]-1, 
+                                        z, z + blk_size[2]-1]).astype(int)
                     
-
-                    temp_hr = np.array([ranges_hr[0][ii], ranges_hr[0][ii]+blk_size_hr[0]-1,
-                                        ranges_hr[1][jj], ranges_hr[1][jj]+blk_size_hr[1]-1,
-                                        ranges_hr[2][kk], ranges_hr[2][kk]+blk_size_hr[2]-1]).astype(int)
-
+                    x,y,z = ranges_hr[0][ii],ranges_hr[1][jj],ranges_hr[2][kk]
+                    temp_hr = np.array([x, x + blk_size_hr[0]-1,
+                                        y, y + blk_size_hr[1]-1,
+                                        z, z + blk_size_hr[2]-1]).astype(int)
+                    
+                    
                     curr_blk = datalr[temp_lr[0]:temp_lr[1]+1, temp_lr[2]:temp_lr[3]+1, temp_lr[4]:temp_lr[5]+1, ...]
                     curr_blk_hr = datahr[temp_hr[0]:temp_hr[1]+1, temp_hr[2]:temp_hr[3]+1, temp_hr[4]:temp_hr[5]+1, ...]
-                    # print(curr_blk.size)
-                    # print(curr_blk.size,curr_blk.shape,curr_blk_hr.shape,curr_blk_hr.size)
-                    if(curr_blk.size != 0 and curr_blk_hr.size != 0):
-                        if((np.count_nonzero(curr_blk)/curr_blk.size > thres) and (np.count_nonzero(curr_blk_hr)/curr_blk_hr.size > thres)):
-                            ind_block_lr.append(temp_lr)
-                            ind_block_hr.append(temp_hr)
-                            count = count + 1
-
-        
+                    # print(curr_blk.shape,curr_blk_hr.shape)
+                    if((torch.numel(curr_blk) != 0 and (torch.count_nonzero(curr_blk).item()/torch.numel(curr_blk)) > self.thres) and 
+                      (torch.numel(curr_blk_hr) != 0 and (torch.count_nonzero(curr_blk_hr).item()/torch.numel(curr_blk_hr)) > self.thres)):
+                        ind_block_lr.append(temp_lr)
+                        ind_block_hr.append(temp_hr)
+                        count = count + 1
+                    
+        # print(blk_size_hr)
         ind_block_lr = np.stack(ind_block_lr)
         ind_block_lr = ind_block_lr.astype(int)
         ind_block_hr = np.stack(ind_block_hr)
         ind_block_hr = ind_block_hr.astype(int)
+        # print(ind_block)
         return ind_block_lr,ind_block_hr,len(ind_block_lr)
 
-    def extract_block(self,data, inds_lr,inds_hr,interpolate = False):
+
+    def extract_block(self,data, inds_lr,inds_hr):
         blocks = []
         ind_blk = []
         for ii in np.arange(inds_lr.shape[0]):
             inds_this = inds_lr[ii, :]
             curr_blk = data[inds_this[0]:inds_this[1]+1, inds_this[2]:inds_this[3]+1, inds_this[4]:inds_this[5]+1, ...]
-            if(interpolate):
-                curr_blk = torch.permute(torch.from_numpy(curr_blk), (3,0,1,2))
-                curr_blk = torch.unsqueeze(curr_blk, 0)
-                curr_blk = torch.nn.functional.interpolate(curr_blk,size = torch.Size(self.blk_size[:3]))
-                curr_blk = torch.permute(curr_blk, (2,3,4,1,0))
-                curr_blk = torch.squeeze(curr_blk)
+            # print(curr_blk.shape)
             blocks.append(curr_blk)
             ind_blk.append(inds_hr[ii,:])
 
         return np.stack(blocks, axis=0),np.stack(ind_blk,axis=0)
 
     def pre_proc(self,idx):
+        
+        vol = torch.from_numpy(loaded[idx]['vol0'])
+        size = [int(self.scale[i] * vol.shape[i]) for i in range(3)]
+        
+        # print(size,self.scale)
+        vol_hr = interpolate(torch.from_numpy(loaded_gt[idx]['vol0']),size)
+        mask_lr = torch.from_numpy(loaded[idx]['mask'])
+        mask_hr = interpolate(torch.from_numpy(loaded_gt[idx]['mask']),size)
 
-        vol = loaded[idx]['vol0']
-        mask_lr = loaded[idx]['mask']
-        mask_hr = loaded_gt[idx]['mask']
-        adc = loaded_gt[idx]['ADC']
-        fa = loaded_gt[idx]['FA']
-        rgb = loaded_gt[idx]['color_FA']
 
+        adc = interpolate(torch.from_numpy(loaded_gt[idx]['ADC']),size)
+        fa = interpolate(torch.from_numpy(loaded_gt[idx]['FA']),size)
+        rgb = interpolate(torch.from_numpy(loaded_gt[idx]['color_FA']),size)
 
-        vol_norm = (vol-np.min(vol))/(np.max(vol)-np.min(vol))
+        vol_norm = (vol-torch.min(vol))/(torch.max(vol)-torch.min(vol))
         curr_blk = self.blk_points_pair(mask_lr,mask_hr,self.blk_size,self.scale,self.stride)
         
         
-        blks_img,curr_blk = self.extract_block(vol_norm,curr_blk[0],curr_blk[1],interpolate=True)
+        blks_img_hr,_ = self.extract_block(vol_hr,curr_blk[1],curr_blk[1])
+        blks_img,curr = self.extract_block(vol_norm,curr_blk[0],curr_blk[1])
         
-        return blks_img,adc,fa,rgb,curr_blk,mask_hr
+        return blks_img,adc,fa,rgb,curr,blks_img_hr,curr_blk[0]
